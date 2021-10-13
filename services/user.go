9c26f98e9cfd0ffc50adaa3c7e77c30f89e3a8c4
Ayrton Souza <ayrtonsouza@icloud.com>
2021-10-13 16:00:24 -0300

    [:construction:] creating client stream method in server

--- services/user.go
+++ services/user.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/codeedu/fc2-grpc/pb"
@@ -61,4 +63,28 @@ func (*UserService) AddUserVerbose(request *pb.User, stream pb.UserService_AddUs
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		request, error := stream.Recv()
+		if error == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if error != nil {
+			log.Fatalf("Error receiving stream: %v", error)
+		}
+
+		users = append(users, &pb.User{
+			Id:    request.GetId(),
+			Name:  request.GetName(),
+			Email: request.GetEmail(),
+		})
+		fmt.Println("Adding", request.GetName())
+	}
 }
